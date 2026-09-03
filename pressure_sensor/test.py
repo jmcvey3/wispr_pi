e4c0ed68caa6eb52537e848812b54b8d15036013
jmcvey3 <53623232+jmcvey3@users.noreply.github.com>
2026-09-03 12:04:52 -0700

    Add test script for pressure sensor and make initiation more robust

--- pressure_sensor/test.py
+++ pressure_sensor/test.py
@@ -1,6 +1,4 @@
-import os
 import time
-import logging
 from datetime import datetime, timezone
 import ms5837
 
@@ -11,27 +9,6 @@ sensor_read_time = (
 burst_seconds = 60
 pressureFreq = 4
 pressure_samples = pressureFreq * burst_seconds
-
-data_path = "/media/wispr_sd/pressure_sensor/data"
-log_path = "/media/wispr_sd/pressure_sensor/logs"
-
-os.makedirs(log_path, exist_ok=True)
-
-# Set up logging file
-now = datetime.now(timezone.utc)
-logger = logging.getLogger("system_logger")
-LOG_FILE = os.path.join(
-    log_path,
-    "pressure_sensor." + datetime.strftime(now, "%Y%m%d") + ".log",
-)
-logging.basicConfig(
-    filename=LOG_FILE,
-    format="%(asctime)s, %(filename)s - [%(levelname)s] - %(message)s",
-    level=logging.DEBUG,
-)
-logging.info("-------------------------------")
-logging.info("Starting tdh_pressure.py")
-
 
 # Initialize pressure sensor
 sensor = ms5837.MS5837_30BA()  # Default I2C bus is 1
@@ -47,12 +24,11 @@ try:
             break
         time.sleep(1)
     if not initiated:
-        logging.error("Sensor could not be initialized")
-        exit(1)
+        raise ValueError("Sensor could not be initialized")
 except Exception as e:
-    logging.error("Error initializing pressure sensor")
-    logging.error(e)
-    exit(1)
+    print(e)
+    raise ValueError("Error initializing pressure sensor")
+print("Sensor intiated")
 
 # Perform a read to populate the sensor's internal state and report any errors
 try:
@@ -65,20 +41,17 @@ try:
             break
         time.sleep(1)
     if not read:
-        logging.error("Sensor read failed!")
-        exit(1)
+        raise ValueError("Sensor read failed!")
 except Exception as e:
-    logging.error("Error reading pressure sensor data")
-    logging.error(e)
-    exit(1)
+    print(e)
+    raise ValueError("Error reading pressure sensor data")
+print("Sensor read functional")
 
-logging.info("Pressure sensor initialized successfully")
-
+# Main loop
 while True:
     # Collect one burst into memory.
     now = datetime.now(timezone.utc)
-    fname = "pressure_sensor." + datetime.strftime(now, "%Y%m%d.%H%M%S") + ".csv"
-    logging.info("Collecting burst: %s" % fname)
+    timestamp = datetime.strftime(now, "%Y%m%d.%H%M%S")
 
     rows = ["timestamp,pressure_dbar,temperature_C\n"]
     isample = 0
@@ -91,18 +64,10 @@ while True:
         except Exception as e:
             P = -9999
             T = -9999
-            logging.error("Error reading pressure sensor")
-            logging.error(e)
 
         timestr = "{:%Y-%m-%d %H:%M:%S.%f}".format(timestamp)
         rows.append("%s,%f,%f\n" % (timestr, P, T))
         isample += 1
         time.sleep(1 / pressureFreq - sensor_read_time)
 
-    logging.info("Burst complete — writing to SD card")
-    try:
-        with open(os.path.join(data_path, fname), "w", newline="\n") as f_out:
-            f_out.writelines(rows)
-        logging.info("Write complete: %s" % fname)
-    except Exception as e:
-        logging.error("Error writing burst file: %s" % e)
+        print(f"{timestamp}, pressure={P}, temperature={T}")
